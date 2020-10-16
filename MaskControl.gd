3b632fec8f4ba6bc2cf0ab20a0c74a8c1f611b21
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-10-16 07:06:46 -0700

    Control node with only one mask texture prototype.

--- MaskControl.gd
+++ MaskControl.gd
@@ -2,11 +2,13 @@ extends Control
 class_name MaskControl
 
 var mask_material:ShaderMaterial = load("res://MaskedMaterial.tres")
+export var mask_texture:Texture 
 
 func _process(_time: float):
 	var local_xform: Transform2D = Transform2D(Vector2(rect_size.x,0.0), Vector2(0.0,rect_size.y),Vector2(0.5,0.5))
 	var mask_to_world_xform: Transform2D = (get_viewport_transform() * get_global_transform() * local_xform)
 	mask_material.set_shader_param("mask_transform", mask_to_world_xform.affine_inverse())
+	mask_material.set_shader_param("mask_texture", mask_texture)
 	update_child_materials(self, mask_material)
 
 func update_child_materials(p_node, p_mat):
@@ -17,9 +19,3 @@ func update_child_materials(p_node, p_mat):
 			var control:Control = N
 			if control.material == null:
 				control.material = p_mat
-			elif control.material.next_pass and control.material.next_pass != p_mat:
-				recurse_next_pass(control.material)
-
-func recurse_next_pass(p_mat):
-	# TODO
-	pass
